b493dcfe8f2a9a5427b13a7465a616fb5ca5c698
wormeyman <wormeyman@gmail.com>
2014-06-15 19:03:54 -0700

    Cleanup

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
-<?php //Opening PHP tag
-
-/*
- *  Add responsive container to embeds
- */ 
-function er_embed_html( $html ) {
-    return '<div class="video-container">' . $html . '</div>';
-}
-    add_filter( 'embed_oembed_html', 'er_embed_html', 10, 3 );
-    add_filter( 'video_embed_html', 'er_embed_html' ); // Jetpack
+<?php //Opening PHP tag
+
+/*
+ *  Add responsive container to embeds
+ */
+function er_embed_html( $html ) {
+    return '<div class="video-container">' . $html . '</div>';
+}
+    add_filter( 'embed_oembed_html', 'er_embed_html', 10, 3 );
+    add_filter( 'video_embed_html', 'er_embed_html' ); // Jetpack
 ?>
