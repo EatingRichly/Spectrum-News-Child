125f3bba1c3a80d1526e5a192eabf63069e31ff9
wormeyman <wormeyman@gmail.com>
2014-06-15 19:03:28 -0700

    Cleanup and remove </meta>

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head profile="http://gmpg.org/xfn/11">
 
-<meta name="readability-verification" content="vkbsy3e9WbsYyJa56J5h8NTf9SVLKznNKsZ5FMQP"/>
-<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport"></meta>
+<meta name="readability-verification" content="vkbsy3e9WbsYyJa56J5h8NTf9SVLKznNKsZ5FMQP" />
+<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 <title><?php woo_title(); ?></title>
@@ -12,8 +12,8 @@
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
 <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php $feedurl = get_option('woo_feed_url'); if ( $feedurl ) { echo $feedurl; } else { echo get_bloginfo_rss('rss2_url'); } ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-      
-      
+
+
 
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' );  ?>
 
@@ -27,21 +27,21 @@
 <?php if ( !$paged && get_option('woo_featured') == "true" ) { ?>
 <script type="text/javascript">
 jQuery(window).load(function(){
-	jQuery("#loopedSlider").loopedSlider({
-	<?php
-		$autoStart = 0;
-		$slidespeed = 600;
-		if ( get_option("woo_slider_auto") == "true" ) 
-		   $autoStart = get_option("woo_slider_interval") * 1000;
-		else 
-		   $autoStart = 0;
-		if ( get_option("woo_slider_speed") <> "" ) 
-			$slidespeed = get_option("woo_slider_speed") * 1000;
-	?>
-		autoStart: <?php echo $autoStart; ?>, 
-		slidespeed: <?php echo $slidespeed; ?>, 
-		autoHeight: true
-	});
+    jQuery("#loopedSlider").loopedSlider({
+    <?php
+        $autoStart = 0;
+        $slidespeed = 600;
+        if ( get_option("woo_slider_auto") == "true" )
+           $autoStart = get_option("woo_slider_interval") * 1000;
+        else
+           $autoStart = 0;
+        if ( get_option("woo_slider_speed") <> "" )
+            $slidespeed = get_option("woo_slider_speed") * 1000;
+    ?>
+        autoStart: <?php echo $autoStart; ?>,
+        slidespeed: <?php echo $slidespeed; ?>,
+        autoHeight: true
+    });
 });
 </script>
 <?php } ?>
@@ -103,61 +103,61 @@ jQuery(window).load(function(){
 <div id="wrapper">
 <div id="background">
 
-	<div id="top-nav" class="col-full">
-		<?php
-		if ( function_exists('has_nav_menu') && has_nav_menu('primary-menu') ) {
-			wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_class' => 'nav', 'theme_location' => 'primary-menu' ) );
-		} else {
-		?>
+    <div id="top-nav" class="col-full">
+        <?php
+        if ( function_exists('has_nav_menu') && has_nav_menu('primary-menu') ) {
+            wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_class' => 'nav', 'theme_location' => 'primary-menu' ) );
+        } else {
+        ?>
         <ul class="nav">
-			<?php 
-        	if ( get_option('woo_custom_nav_menu') == 'true' ) {
-        		if ( function_exists('woo_custom_navigation_output') )
-					woo_custom_navigation_output('name=Woo Menu 1&depth=6');
+            <?php
+            if ( get_option('woo_custom_nav_menu') == 'true' ) {
+                if ( function_exists('woo_custom_navigation_output') )
+                    woo_custom_navigation_output('name=Woo Menu 1&depth=6');
 
-			} else { ?>
-            	
-            	
+            } else { ?>
 
-	            <?php if ( is_home() OR is_front_page()) $highlight = "page_item current_page_item"; else $highlight = "page_item"; ?>
-	            <li class="<?php echo $highlight; ?>"><a href="<?php bloginfo('url'); ?>"><?php _e('Home', 'woothemes') ?></a></li>
-	            <?php wp_list_pages('sort_column=menu_order&depth=6&title_li=&exclude='.get_option('woo_pages_exclude')); ?>
-	            
-	            
+
+
+                <?php if ( is_home() OR is_front_page()) $highlight = "page_item current_page_item"; else $highlight = "page_item"; ?>
+                <li class="<?php echo $highlight; ?>"><a href="<?php bloginfo('url'); ?>"><?php _e('Home', 'woothemes') ?></a></li>
+                <?php wp_list_pages('sort_column=menu_order&depth=6&title_li=&exclude='.get_option('woo_pages_exclude')); ?>
+
+
 
            <?php } ?>
         </ul><!-- /#nav -->
-        <?php } ?>       
+        <?php } ?>
 
        <span class="nav-item-right">
 
-	   <a href="<?php $feedurl = get_option('woo_feed_url'); if ( $feedurl ) { echo $feedurl; } else { echo get_bloginfo_rss('rss2_url'); } ?>">
+       <a href="<?php $feedurl = get_option('woo_feed_url'); if ( $feedurl ) { echo $feedurl; } else { echo get_bloginfo_rss('rss2_url'); } ?>">
 
-	   <img src="http://eatingrichly.com/wp-content/uploads/2014/02/ico-rss.png" height="16" width="16" alt="RSS Feed" />
+       <img src="http://eatingrichly.com/wp-content/uploads/2014/02/ico-rss.png" height="16" width="16" alt="RSS Feed" />
 
-	   </a>
+       </a>
 
-	   <a href="https://www.facebook.com/eatingrichly">
+       <a href="https://www.facebook.com/eatingrichly">
 
-			<img src="http://eatingrichly.com/wp-content/uploads/2014/02/fb-2014-16x16.png" height="16" width="16" alt="Facebook" />
+            <img src="http://eatingrichly.com/wp-content/uploads/2014/02/fb-2014-16x16.png" height="16" width="16" alt="Facebook" />
 
-		</a>
+        </a>
 
-		<a href="http://twitter.com/eatingrichly">
+        <a href="http://twitter.com/eatingrichly">
 
-			<img src="http://eatingrichly.com/wp-content/uploads/2014/02/Twitter_logo_blue-16X13.png" height="13" width="16" alt="Twitter Feed" />
+            <img src="http://eatingrichly.com/wp-content/uploads/2014/02/Twitter_logo_blue-16X13.png" height="13" width="16" alt="Twitter Feed" />
 
-		</a>
+        </a>
 
-		<a href="http://pinterest.com/eatingrichly" style="padding-right:5px;">
+        <a href="http://pinterest.com/eatingrichly" style="padding-right:5px;">
 
-			<img src="http://eatingrichly.com/wp-content/uploads/2014/02/Pinterest_Favicon-16x16.png" height="16" width="16" alt="Pinterest" />
+            <img src="http://eatingrichly.com/wp-content/uploads/2014/02/Pinterest_Favicon-16x16.png" height="16" width="16" alt="Pinterest" />
 
-		</a>
+        </a>
 
-	   </span>
+       </span>
 
-<div class="ericssearchformintheheader">		
+<div class="ericssearchformintheheader">
 
 <form method="get" class="searchform" action="<?php bloginfo('url'); ?>" >
 
@@ -169,27 +169,27 @@ jQuery(window).load(function(){
 
     </form>
 
-</div><!--/ericssearchformintheheader-->	   
+</div><!--/ericssearchformintheheader-->
 
-	</div><!-- /#top-nav -->
-           
-           
+    </div><!-- /#top-nav -->
 
-	<div id="header" class="col-full">
- 		       
- 		       
 
-		<div id="logo">
-	       
-	       
 
-		<?php if (get_option('woo_texttitle') <> "true") : $logo = get_option('woo_logo'); ?>
+    <div id="header" class="col-full">
+
+
+
+        <div id="logo">
+
+
+
+        <?php if (get_option('woo_texttitle') <> "true") : $logo = get_option('woo_logo'); ?>
             <a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('description'); ?>">
                 <img src="<?php if ($logo) echo $logo; else { bloginfo('template_directory'); ?>/images/logo.png<?php } ?>" alt="<?php bloginfo('name'); ?>" />
             </a>
-        <?php endif; ?> 
-        
-        
+        <?php endif; ?>
+
+
 
         <?php if( is_singular() ) : ?>
             <span class="site-title"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></span>
@@ -197,52 +197,51 @@ jQuery(window).load(function(){
             <h1 class="site-title"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
         <?php endif; ?>
             <span class="site-description"><?php bloginfo('description'); ?></span>
-	      	
-	      	
 
-		</div><!-- /#logo -->
-	       
-	       
 
-		<?php if (get_option('woo_ad_top') == 'true') { ?>
+
+        </div><!-- /#logo -->
+
+
+
+        <?php if (get_option('woo_ad_top') == 'true') { ?>
         <div id="topad">
-	    	<?php include (TEMPLATEPATH . "/ads/top_ad.php"); ?>
-	   	</div><!-- /#topad -->
+            <?php include (TEMPLATEPATH . "/ads/top_ad.php"); ?>
+           </div><!-- /#topad -->
         <?php } ?>
-       
-       
 
-	</div><!-- /#header -->
-    
-    
 
-	<div id="main-nav" class="col-full">
-		<?php
-		if ( function_exists('has_nav_menu') && has_nav_menu('secondary-menu') ) {
-			wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_class' => 'nav', 'theme_location' => 'secondary-menu' ) );
-		} else {
-		?>
+
+    </div><!-- /#header -->
+
+
+
+    <div id="main-nav" class="col-full">
+        <?php
+        if ( function_exists('has_nav_menu') && has_nav_menu('secondary-menu') ) {
+            wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_class' => 'nav', 'theme_location' => 'secondary-menu' ) );
+        } else {
+        ?>
         <ul class="nav">
-			<?php 
-        	if ( get_option('woo_custom_nav_menu') == 'true' ) {
-        		if ( function_exists('woo_custom_navigation_output') )
-					woo_custom_navigation_output('name=Woo Menu 2&depth=6');
+            <?php
+            if ( get_option('woo_custom_nav_menu') == 'true' ) {
+                if ( function_exists('woo_custom_navigation_output') )
+                    woo_custom_navigation_output('name=Woo Menu 2&depth=6');
 
-			} else { ?>
-            	
-            	
+            } else { ?>
 
-	            <?php if ( is_home() OR is_front_page()) $highlight = "page_item current_page_item"; else $highlight = "page_item"; ?>
-	            <li class="<?php echo $highlight; ?>"><a href="<?php bloginfo('url'); ?>"><?php _e('Home', 'woothemes') ?></a></li>
-	            <?php wp_list_categories('sort_column=menu_order&depth=6&title_li=&exclude='.get_option('woo_cats_exclude')); ?>
-	            
-	            
 
-	        <?php } ?>
-	            
-	            
+
+                <?php if ( is_home() OR is_front_page()) $highlight = "page_item current_page_item"; else $highlight = "page_item"; ?>
+                <li class="<?php echo $highlight; ?>"><a href="<?php bloginfo('url'); ?>"><?php _e('Home', 'woothemes') ?></a></li>
+                <?php wp_list_categories('sort_column=menu_order&depth=6&title_li=&exclude='.get_option('woo_cats_exclude')); ?>
+
+
+
+            <?php } ?>
+
+
 
         </ul><!-- /#nav -->
-        <?php } ?>       
-	</div><!-- /#main-nav -->
-       
+        <?php } ?>
+    </div><!-- /#main-nav -->
