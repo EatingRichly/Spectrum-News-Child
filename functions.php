362ea7a83db9a614c70331b0eaf173d159bb2696
wormeyman <wormeyman@gmail.com>
2014-06-20 20:52:09 -0700

    add er enque to functions.php

--- functions.php
+++ functions.php
@@ -8,4 +8,23 @@ function er_embed_html( $html ) {
 }
     add_filter( 'embed_oembed_html', 'er_embed_html', 10, 3 );
     add_filter( 'video_embed_html', 'er_embed_html' ); // Jetpack
+/*
+ *  End Add responsive container to embeds
+ */
+/**
+ * Proper way to enqueue scripts and styles
+ */
+function custom_styles() {
+
+    wp_register_style( 'er_child_theme', 'get_stylesheet_uri()', false, '1.0.0', 'all' );
+    wp_enqueue_style( 'er_child_theme' );
+}
+
+// Hook into the 'wp_enqueue_scripts' action
+add_action( 'wp_enqueue_scripts', 'custom_styles' );
+/**
+ * End Proper way to enqueue scripts and styles
+ */
+
+
 ?>
